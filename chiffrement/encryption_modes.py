b30e51095c6d5346de778a4b1a8bca507a9bdd06
cuckool <32191850+cuckool@users.noreply.github.com>
2018-12-25 18:19:48 +0100

    Add files via upload

---
+++ chiffrement/encryption_modes.py
@@ -0,0 +1,109 @@
+import random
+
+
+def ecb_encryption(plain_text, enc_key, encryption_function):
+    """
+    Une entrée de la liste plain_text >> un bloc de 64 bits
+    """
+    cypher_txt = []
+    for i in plain_text:
+        cypher_txt.append(encryption_function(enc_key, i))
+    return cypher_txt
+
+
+def ecb_decryption(cypher_txt, dec_key, decryption_function):
+    plain_text = []
+    for i in cypher_txt:
+        plain_text.append(decryption_function(dec_key, i))
+    return plain_text
+
+
+def cbc_encryption(plain_text, enc_key, encryption_function):
+    """
+    Retourne une list avec un nb de plus en [0], le bloc qui ne pourra pas être décodé sans l'IV
+    Une entrée de la liste plain_text >> un bloc de 64 bits
+    """
+    cypher_txt = []
+    plain_text_cp = plain_text[:]
+    plain_text_cp.insert(0, random.randrange(0, 2**64-1))
+    initialisation_vector = random.randrange(0, 2**64-1)
+    for i in plain_text_cp:
+        temp = initialisation_vector ^ i
+        cypher_bloc = encryption_function(enc_key, temp)
+        initialisation_vector = cypher_bloc
+        cypher_txt.append(cypher_bloc)
+    return cypher_txt
+
+
+def cbc_decryption(cypher_txt, dec_key, decryption_function):
+    plain_text = []
+    for ind, i in enumerate(cypher_txt[1:]):
+        temp = decryption_function(dec_key, i)
+        res = temp ^ cypher_txt[ind]                #ça devrait être ind-1 mais, enumerate est sur cypher_txt[1:], donc le index de cet enumerate est le ind-1 de enumerate(cypher)
+        plain_text.append(res)
+    return plain_text
+
+
+def pcbc_encryption(plain_text, enc_key, encryption_function):
+    """
+    Une entrée de la liste plain_text >> un bloc de 64 bits
+    """
+    cypher_txt = []
+    initialisation_vector = enc_key #Comment déduire l'IV de la clé sans pouvoir la retrouver depuis lui ?? Hash de 64 bits ??
+    for index, block in enumerate(plain_text):
+        temp = initialisation_vector ^ block
+        cypher_bloc = encryption_function(enc_key, temp)
+        cypher_txt.append(cypher_bloc)
+        initialisation_vector = block ^ cypher_bloc
+    return cypher_txt
+
+
+def pcbc_decryption(cypher_txt, dec_key, decryption_function):
+    """
+        Une entrée de la liste plain_text >> un bloc de 64 bits
+    """
+    plain_text = []
+    initialisation_vector = dec_key
+    for ind, block in enumerate(cypher_txt):
+        temp = decryption_function(dec_key, block)
+        plain_block = initialisation_vector ^ temp
+        plain_text.append(plain_block)
+        initialisation_vector = block ^ plain_block
+    return plain_text
+
+
+def cfb_encryption(plain_text, enc_key, encryption_function):
+    """
+    Retourne une list avec un nb de plus en [0], le bloc qui ne pourra pas être décodé sans l'IV
+    Une entrée de la liste plain_text >> un bloc de 64 bits
+    """
+    cypher_txt = []
+    plain_text_cp = plain_text[:]
+    plain_text_cp.insert(0, random.randrange(0, 2**64-1))
+    initialisation_vector = random.randrange(0, 2**64-1)
+    for block in plain_text_cp:
+        temp = encryption_function(enc_key, initialisation_vector)
+        cypher_block = block ^ temp
+        initialisation_vector = cypher_block
+        cypher_txt.append(cypher_block)
+    return cypher_txt
+
+
+def cfb_decryption(cypher_txt, dec_key, encryption_function):
+    plain_text = []
+    for ind, i in enumerate(cypher_txt[1:]):
+        temp = encryption_function(dec_key, cypher_txt[ind]) #ça devrait être ind-1 mais, enumerate est sur cypher_txt[1:], donc le index de cet enumerate est le ind-1 de enumerate(cypher)
+        res = temp ^ i
+        plain_text.append(res)
+    return plain_text
+
+
+def test_function(key, plaintext):
+    """
+    Doit respecter la forme:
+     entrée : int sur 64 bits (max 2**64-1) + key de 64 bits
+     retourne : le bloc chiffré
+    """
+    return key ^ plaintext
+
+
---
+++ chiffrement/main.py
@@ -0,0 +1,55 @@
+# from chiffrement.encryption_modes import *
+from chiffrement.file_management import read_files_v2, write_file
+from chiffrement.idea import idea_encryption, idea_decryption
+
+
+def encrypt_file(file_name, new_file_name, mode_of_operation, enc_key):
+    blocks = read_files_v2(file_name)
+    blocks = mode_of_operation(blocks, enc_key, idea_encryption)
+    write_file(new_file_name, blocks)
+
+
+def decrypt_file(file_name, new_file_name, mode_of_operation, dec_key):
+    blocks = read_files_v2(file_name)
+    blocks = mode_of_operation(blocks, dec_key, idea_decryption)
+    write_file(new_file_name, blocks)
+
+
+# if __name__ == '__main__':
+#     clef = random.randrange(0, 2**64-1)
+#     encrypt_file(r'D:\Users\Crowbar\PycharmProjects\GS15\w a t.jpg',
+#                  r'D:\Users\Crowbar\PycharmProjects\GS15\w a t_chiffré.txt',
+#                  encryption_modes.cbc_encryption, enc_key=clef)
+#     decrypt_file(r'D:\Users\Crowbar\PycharmProjects\GS15\w a t_chiffré.txt',
+#                  r'D:\Users\Crowbar\PycharmProjects\GS15\w a t déchiffré.jpg',
+#                  encryption_modes.cbc_decryption, dec_key=clef)
+
+
+
+
+
+# if __name__ == '__main__':
+#     print("""Quelles opérations souhaitez vous faire ?\n
+#     ->1<- Générer une clé publique / privée.\n
+#     ->2<- Authentifier une clé publique / un certificat.\n
+#     ->3<- Partager une clé secrète.\n
+#     ->4<- Utiliser une clé secrète pour chiffrer un message (et le signer).\n
+#     ->5<- Déchiffrer un message et vérifier la signature.\n
+#     ->6<- Complet, salade, tomate oignon.""")
+#     while 1:
+#         choix = input("Choississez une option.")
+#         if choix == 1:
+#             pass
+#         elif choix == 2:
+#             pass
+#         elif choix == 3:
+#             pass
+#         elif choix == 4:
+#             pass
+#         elif choix == 5:
+#             pass
+#         elif choix == 6:
+#             pass
+#         else:
+#             print("Entrez un chiffre entre 1 et 6 svp.")
+

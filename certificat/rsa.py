8d975309f4c56898d4f48d3c2b32c11e5fd3480d
cuckool <32191850+cuckool@users.noreply.github.com>
2019-01-05 16:42:09 +0100

    Add files via upload

--- certificat/rsa.py
+++ certificat/rsa.py
@@ -1,11 +1,10 @@
 import random
 from Crypto.Util import number
 from sympy import mod_inverse
-import hashlib
+import main_pack.hashing_sha3
 
 
 #LES CLEFS AURONT CETTE STRUCTURES POUR RSA !
-#ON NE PEUT PAS RETROUVER PHIN AVEC JUSTE N ?
 
 def key_gen(key_size):
     """ Return : clef_publique, clef_privee : (n, e), (p, q, d)"""
@@ -27,10 +26,11 @@ def rsa_cipher(msg, n, exposant):
 
 def verifier_clef(clef_signe, signature, c_publique):
     """Utilise la clef publique de l'authorité pour vérifier la signature."""
-    empreinte = ''
-    for i in clef_signe:
-        empreinte += str(i)
-    empreinte = hash(empreinte) #UTILISER LA FONCTION DE HASH DE MARTIN
+    # empreinte = ''
+    # for i in clef_signe:
+    #     empreinte += str(i)
+    # # empreinte = hash(empreinte) # UTILISER LA FONCTION DE HASH DE MARTIN
+    empreinte = key_to_hash(clef_signe)
     if rsa_cipher(signature, c_publique[0], c_publique[1]) == empreinte:
         return True
     else:
@@ -39,11 +39,19 @@ def verifier_clef(clef_signe, signature, c_publique):
 
 def signer_clef(clef_a_signer, clef_privee):
     """ Signe une clef publique. Comme dans l'énoncé, la clef a signer est en 3 partie (A, alfa, p)"""
-    empreinte = ''
-    for i in clef_a_signer:
-        empreinte += str(i)
-    empreinte = hash(empreinte) #UTILISER LA FONCTION DE HASH DE MARTIN
+    # empreinte = ''
+    # for i in clef_a_signer:
+    #     empreinte += str(i)
+    # empreinte = hash(empreinte) # UTILISER LA FONCTION DE HASH DE MARTIN
+    empreinte = key_to_hash(clef_a_signer)
     return rsa_cipher(empreinte, clef_privee[0]*clef_privee[1], clef_privee[2])
+
+
+def key_to_hash(clef_publique_a_signe):
+    empreinte = ''
+    for i in clef_publique_a_signe:
+        empreinte += str(i)
+    return int(main_pack.hashing_sha3.hash_document(format(int(empreinte), 'b')), 2)
 
 
 def verifier_hash(empreinte, signature, c_publique):
@@ -57,14 +65,13 @@ def signer_hash(empreinte, clef_privee):
     return rsa_cipher(empreinte, clef_privee[0] * clef_privee[1], clef_privee[2])
 
 
-
 if __name__ == '__main__':
+    pass
     # clef_signer = (66545456, 6156, 11348949556)
-    clef_signer = (random.randrange(2 ** 512 - 1), random.randrange(2 ** 512 - 1), random.randrange(2 ** 512 - 1))
-    clef_publique, clef_privee = key_gen(1024)
-    signature = signer_clef(clef_signer, clef_privee)
-    print(verifier_clef(clef_signer, signature, clef_publique))
-
+    # clef_signer = (random.randrange(2 ** 512 - 1), random.randrange(2 ** 512 - 1), random.randrange(2 ** 512 - 1))
+    # clef_publique, clef_privee = key_gen(1024)
+    # signature = signer_clef(clef_signer, clef_privee)
+    # print(verifier_clef(clef_signer, signature, clef_publique))
 
     # cipher_m = rsa_cipher(clear, clef_privee[0]*clef_privee[1], clef_privee[2])
     # decipher = rsa_cipher(cipher_m, clef_publique[0], clef_publique[1])

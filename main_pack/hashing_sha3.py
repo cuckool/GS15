a868af0a22710586512cecff6645b9b6a306dc6d
cuckool <32191850+cuckool@users.noreply.github.com>
2019-01-05 16:43:47 +0100

    Add files via upload

---
+++ main_pack/hashing_sha3.py
@@ -0,0 +1,263 @@
+import numpy as np
+import sys
+import main_pack.toolbox as tb
+import math
+from LFSR import lfsr as lfsr
+
+""
+"Cette fonction prend en entrée un fichier codé en binaire et renvoie en sortie le hash du fichier"
+""
+
+
+def main():
+    """
+
+    :return:
+    """
+
+    print("""HASHAGE : SHA3""")
+    hash_length, size_c, size_block, size_r = initialisation()
+    doc = tb.load_input()
+    docbin = str(tb.str_to_bin(doc))
+    doc_w_padding = str(tb.padding(size_r, docbin))
+    n_ite = int(len(doc_w_padding)/size_r)
+    hash_hex, hash_bin = hashage(doc_w_padding, n_ite, size_c, size_r, hash_length)
+    print("Le hash SHA3 du fichier est : ")
+    print(hash_hex)
+
+
+def hash_document(doc_bin):
+    """
+
+    :param doc_bin:
+    :return:
+    """
+    hash_length, size_c, size_block, size_r = initialisation()
+    doc_w_padding = str(tb.padding(size_r, doc_bin))
+    n_ite = int(len(doc_w_padding) / size_r)
+    hash_hex, hash_bin = hashage(doc_w_padding, n_ite, size_c, size_r, hash_length)
+    print("Le hash SHA3 du fichier est : ")
+    print(hash_hex)
+    return hash_bin
+
+
+def initialisation():
+    """
+    :return: hash_length, size_c, size_block, size_r
+    """
+    #PAR DEFAUT : hash_length = 384 bits
+    hash_length = 384
+    while True:
+        print("""CALCUL DU HASH DU FICHIER\n
+        Quelles taille de hash voulez vous ?\n
+        ->1<- 256 bits\n
+        ->2<- 384 bits\n
+        ->3<- 512 bits\n """)
+        choix = int(input("Choississez une option."))
+
+        if choix == 1:
+            hash_length = 256
+            break
+
+        elif choix == 2:
+            break
+
+        elif choix == 3:
+            hash_length = 512
+            break
+        else:
+            print("Mauvais choix : on utilise 384 bits par défaut")
+            break
+
+    size_c = 2 * hash_length
+    size_block = 1600
+    size_r = size_block - size_c
+    return hash_length, size_c, size_block, size_r
+
+
+def hashage(doc_w_padding, n_ite, size_c, size_r, hash_length):
+    """
+    :param hash_length:
+    :param doc_w_padding:
+    :param n_ite:
+    :param size_c:
+    :param size_r:
+    :return:
+    """
+    """
+    Création premier bloc = que des 0
+    Initialisation du LFSR
+    """
+    block_hashing_zeroes = [[["0" for k in range(64)] for j in range(5)] for i in range(5)]
+    block_hashing_init = np.asarray(block_hashing_zeroes)
+    string_hashing = array_to_string(block_hashing_init)
+
+    state = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+    L = lfsr.LFSR(fpoly=[8, 4, 3, 2], initstate=state)  # Prime function of degree 8 : x^8 + x^4 + x^3 + x^2 + 1
+
+    """
+    PHASE D'ABSOPTION
+    le nombre d’itération est fixé à n = 24 ;
+    c = 2p.
+    les sous bloc ri et ci sont constitués des premiers et des derniers bits du bloc Bi.
+    """
+    for n in range(0, n_ite):
+        r = string_hashing[:size_r]
+        string_p = doc_w_padding[n * size_r:(n + 1) * size_r]
+
+        r_xor = tb.xor_bin(r, string_p)
+        string_hashing = r_xor + string_hashing[:size_c]
+        block_hashing = string_to_array(string_hashing)
+        for m in range(0, 23):
+            block_hashing, string_hashing, L = hashing_function(block_hashing, L)
+        """
+        6. les sous bloc ri et ci sont respectivement constitués des premiers et des derniers bits du bloc Bi.
+        """
+
+    """
+    PHASE DE RECUPERATION
+    """
+    m = int(math.ceil(hash_length / size_r))
+    r = string_hashing[size_r:]
+    block_hashing, string_hashing, L = hashing_function(block_hashing, L)
+    r_hash = string_hashing[:hash_length]
+    block_hashing, string_hashing, L = hashing_function(block_hashing, L)
+    hash = r_hash
+    """
+    for x in range(1, m):
+        r_hash = string_hashing[size_r:]
+        print(r_hash)
+        block_hashing, string_hashing, L = hashing_function(block_hashing, L)
+        hash = hash + r_hash
+    # print(len(hash))
+    """
+    hash_hex = hex(int(hash, 2))
+    return hash_hex, hash
+
+
+def hashing_function(block_hashing_init, L):
+    """
+
+    :param block_hashing_init:
+    :return:
+    """
+    global k
+    block_hashing_etape1 = block_hashing_init
+
+    """
+    1. on remplace chaque bit de chaque sous-blocs de 64 bits par un XOR avec le bit de parité d’une colone
+    adjacente : B[:; j; k]   B[:; j; k]  parite(B[:; j; k 􀀀 1]) ;
+    """
+    for j in range(0, 5):
+        for k in range(1, 64):
+            block_slice = str(tb.xor_bin(''.join(block_hashing_init[:, j, k]),
+                                         tb.parity_bit(''.join(block_hashing_init[:, j, k - 1]))))
+            block_hashing_etape1[:, j, k] = list(str(block_slice))
+
+        block_slice = tb.xor_bin(''.join(block_hashing_init[:, j, 0]),
+                                 tb.parity_bit(''.join(block_hashing_init[:, j, 63])))
+        block_hashing_etape1[:, j, 0] = list(block_slice)
+
+
+    """
+    2. on permute les blocs de 64 bits de t bits avec t qui dépend de la position dans le tableaux (vous pourrez
+    définir t en fonction des indices (i; j) comme vous le souahitez ; B[i; j; :]   B[i; j; :] << t(i; j) ;
+    """
+    """
+    f_i = 3
+    f_j = 2
+    block_hashing_etape2 = block_hashing_etape1
+    for i in range(0, 5):
+        for j in range(0, 5):
+            block_hashing_etape2[i, j, :] = block_hashing_etape1[tb.addition_mod(f_i, i, 5),
+                                            tb.addition_mod(j, f_j, 5),
+                                            :]
+    print("à l'étape 2 : " + str(array_to_string(block_hashing_etape2)))
+    """
+
+    """
+    3. on permute les sous-blocs de 64 bits du tableau : B[i; j; :]   B[j; 2i+3j; :] (attention à la permutation
+    des lignes et des colones) avec ici, bien sûr, 2i + 3jmod5 ;
+    """
+    block_hashing_etape2 = block_hashing_etape1
+    block_hashing_etape3 = block_hashing_etape2
+    for i in range(0, 5):
+        for j in range(0, 5):
+            block_hashing_etape3[i, j, :] = block_hashing_etape2[j, tb.addition_mod(2 * i, 3 * j, 5), :]
+
+    """
+    4. on effectue un XOR entre les lignes : B[:; j; :]   B[:; j; :]  (B[:; j + 1; :]&B[:; j - 1; :]) ;
+    """
+    block_hashing_etape4 = block_hashing_etape3
+    for j in range(0, 4):
+        for i in range(0, 5):
+            block_slice = tb.xor_bin(''.join(block_hashing_etape3[i, j, :]),
+                                     tb.logical_and(''.join(block_hashing_etape3[i, (j + 1)%5, :]),
+                                                    ''.join(block_hashing_etape3[i, j - 1, :])))
+            block_hashing_etape4[i, j, :] = list(block_slice)
+    block_slice = tb.xor_bin(''.join(block_hashing_etape3[i, j, :]),
+                             tb.logical_and(''.join(block_hashing_etape3[i, 1, :]),
+                                            ''.join(block_hashing_etape3[i, 4, :])))
+    block_hashing_etape4[i, j, :] = list(block_slice)
+    """
+    5. on effectue une dernière opération de XOR entre certains bits des mots de 64 bits : B[j; j; :]  
+    B[j; j; 2m 􀀀 1]  B[j; j;m + 7  L(m)] avec m = f0; : : : ; 6g, 20 􀀀 1 = 􀀀1  63mod64 et L(m)
+    correspondant à la sortie (un bit) d’un LFSR de 8 bits.
+    """
+    block_hashing_etape5 = block_hashing_etape4
+    for m in range(0, 7):
+        for j in range(1, 5):
+            block_hashing_etape5[j, j, m] = tb.xor_bin(block_hashing_etape4[j,j,(2^m-1)%64], block_hashing_etape4[j,j,(m+7*L.next())%64])
+
+    block_hashing_fin = block_hashing_etape5
+    string_hashing = array_to_string(block_hashing_etape5)
+    return block_hashing_fin, string_hashing, L
+
+
+def array_to_string(block_hashing):
+    """
+
+    :param block_hashing:
+    :return:
+    """
+    string_hashing = '0'
+    for i in range(0, 5):
+        for j in range(0, 5):
+            string_hashing = string_hashing + ''.join(block_hashing[i, j, :])
+
+    string_hashing = string_hashing[1:]
+    return string_hashing
+
+
+def string_to_array(string_hashing):
+    """
+
+    :param string_hashing:
+    :return:
+    """
+    block_hashing_tab = [[["0" for y in range(64)] for x in range(5)] for z in range(5)]
+    block_hashing = np.asarray(block_hashing_tab)
+    n = 0
+    for i in range(0, 5):
+        for j in range(0, 5):
+            for k in range(0, 64):
+                block_hashing[i, j, k] = string_hashing[n]
+                n = n + 1
+
+    return block_hashing
+
+
+def m_LFSR(L):
+    """
+
+    :param L:
+    :return:
+    """
+    m = str(L.next())
+    for i in range(1, 8):
+        m = m + str(L.next())
+    return int(m, 2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_pack/menu.py
@@ -0,0 +1,144 @@
+from chiffrement import main, encryption_modes, file_management
+from certificat import rsa
+import os
+from main_pack import hashing_sha3, diffie_hellman_key_sharing
+
+
+
+
+def generation_clef_pri_pu(clef_de_chiffrement_publique, clef_de_chiffrement_privee):
+    if clef_de_chiffrement_privee != None and clef_de_chiffrement_publique != None:
+        choix_changement = int(input("Il y a déjà des clefs de chiffrement publique/priveé. "
+                                     "Voulez-vous en changer ?\n "
+                                     "Vous ne pourrez plus authentifier les documents signés avec le précédent"
+                                     " jeu de clef.\n"
+                                     "1. Pour changer \n2. Pour garder les mêmes clefs."))
+    else:
+        choix_changement = 1
+    if choix_changement == 1:
+        pu_pri_key_length = int(
+            input("Quelle taille (en bits) pour les clefs publique/privées pour la signature ? "
+                  "Recommandation : supérieur a 1024."))
+        return rsa.key_gen(pu_pri_key_length)
+    elif choix_changement == 2:
+        return clef_de_chiffrement_publique, clef_de_chiffrement_privee
+
+
+def chiffrement_fichier(clef_chiffrement_idea, clef_chiffrement_idea_long):
+    if clef_chiffrement_idea is None:
+        fichier_orig, fichier_chif, cipher_mode, clef_chiffrement_idea, clef_chiffrement_idea_long = main.ask_users_var_chiffrement()
+    else:
+        fichier_orig, fichier_chif, cipher_mode = main.ask_user_chiffrement_sans_clef()
+    main.encrypt_file(fichier_orig, fichier_chif, cipher_mode, clef_chiffrement_idea,
+                      clef_chiffrement_idea_long)
+
+    bin_string = file_management.read_in_bin(fichier_orig)
+    empreinte_fichier = int(hashing_sha3.hash_document(bin_string), 2)  #                                 générer clef chiffrement publique/privé > signé le doc#faut implémenter la signature : hash de martin > générer clef chiffrement publique/privé > signé le doc
+    signature_fichier = rsa.signer_hash(empreinte=empreinte_fichier, clef_privee=clef_de_chiffrement_privee)
+    with open(os.path.join(os.path.dirname(fichier_chif), 'signature_fichier.txt'), mode='w') as f:
+        f.write(str(signature_fichier))
+    print("Fichier chiffré avec succès :")
+    print(fichier_chif)
+    print("Signature :")
+    print(os.path.join(os.path.dirname(fichier_chif), 'signature_fichier.txt'))
+    return fichier_chif, cipher_mode, clef_chiffrement_idea, clef_chiffrement_idea_long
+
+
+def dechiffrement_fichier(fichier_a_dechif, fichier_dechif, decipher_mode, option6=False):
+    if not option6:
+        fichier_a_dechif, fichier_dechif, decipher_mode = main.ask_user_dechiffrement()
+    main.decrypt_file(fichier_a_dechif, fichier_dechif, decipher_mode, dec_key=clef_chiffrement_idea,
+                      key_length=clef_chiffrement_idea_long)
+    with open(os.path.join(os.path.dirname(fichier_a_dechif), 'signature_fichier.txt'), mode='r') as f_d:
+        signature_fichier_dechiffre = int(f_d.read())
+
+
+
+    #empreinte_fichier_dechif = 9815616816315616  # FAUT METTRE LE HASH DE MARTIN SUR LE FICHIER DECHIFF ICI
+    bin_string2 = file_management.read_in_bin(fichier_dechif)
+    empreinte_fichier_dechif = int(hashing_sha3.hash_document(bin_string2), 2)
+
+    if rsa.verifier_hash(empreinte_fichier_dechif, signature_fichier_dechiffre, clef_de_chiffrement_publique):
+        print("La signature est valide.")
+    else:
+        print("La signature est invalide !")
+
+
+if __name__ == '__main__':
+    clef_chiffrement_idea = None
+    clef_chiffrement_idea_long = None           # longueur de la clef IDEA
+    clef_de_chiffrement_publique = None
+    clef_de_chiffrement_privee = None
+
+
+    while True:
+        print("""Quelles opérations souhaitez vous faire ?\n
+        ->1<- Générer une clé publique / privée.\n
+        ->2<- Authentifier une clé publique / un certificat.\n
+        ->3<- Partager une clé secrète.\n
+        ->4<- Utiliser une clé secrète pour chiffrer un message (et le signer).\n
+        ->5<- Déchiffrer un message et vérifier la signature.\n
+        ->6<- Complet, salade, tomate oignon.""")
+        choix = int(input("Choississez une option."))
+
+        if choix == 1:
+            clef_de_chiffrement_publique, clef_de_chiffrement_privee = generation_clef_pri_pu(
+                clef_de_chiffrement_publique, clef_de_chiffrement_privee)
+
+        elif choix == 2:
+            if clef_de_chiffrement_privee == None or clef_de_chiffrement_publique == None:
+                print(
+                    "Il n'y a pas de clef de chiffrement privée/publique pour la signature. Elles vont être générées.")
+                pu_pri_key_length = int(
+                    input("Quelle taille (en bits) pour ces clefs ? Recommandation : supérieur a 1024."))
+                clef_de_chiffrement_publique, clef_de_chiffrement_privee = rsa.key_gen(pu_pri_key_length)
+            a, alfa, p = diffie_hellman_key_sharing.generer_clef_publique()
+            signature_clef = int(rsa.signer_clef((a, alfa, p), clef_de_chiffrement_privee))
+            print("Clef :", (a, alfa, p))
+            print('Signature :', signature_clef)
+            print("La signature est vérifiée :",rsa.verifier_clef((a, alfa, p), signature_clef, clef_de_chiffrement_publique))
+
+        elif choix == 3:
+            clef_chiffrement_idea, clef_chiffrement_idea_long = diffie_hellman_key_sharing.generer_clef_secrete()
+            print("Clef générée grâce à Diffie-Hellman.")
+
+        elif choix == 4:
+            if clef_de_chiffrement_privee == None or clef_de_chiffrement_publique == None:
+                print(
+                    "Il n'y a pas de clef de chiffrement privée/publique pour la signature. Elles vont être générées.")
+                pu_pri_key_length = int(
+                    input("Quelle taille (en bits) pour ces clefs ? Recommandation : supérieur a 1024."))
+                clef_de_chiffrement_publique, clef_de_chiffrement_privee = rsa.key_gen(pu_pri_key_length)
+
+            bob, bab, clef_chiffrement_idea, clef_chiffrement_idea_long = chiffrement_fichier(clef_chiffrement_idea,
+                                                                                              clef_chiffrement_idea_long)
+
+        elif choix == 5:
+            dechiffrement_fichier(None, None, None, option6=False)
+
+        elif choix == 6:
+            print("Génération de clef publique/privée pour la signature")
+            clef_de_chiffrement_publique, clef_de_chiffrement_privee = generation_clef_pri_pu(
+                clef_de_chiffrement_publique, clef_de_chiffrement_privee)
+            # =================================================================================================
+            clef_chiffrement_idea, clef_chiffrement_idea_long = diffie_hellman_key_sharing.generer_clef_secrete()
+            print("Clef générée grâce à Diffie-Hellman.")
+            # =================================================================================================
+            fichier_chiffre, cipher_mode, clef_chiffrement_idea, clef_chiffrement_idea_long = chiffrement_fichier(
+                clef_chiffrement_idea, clef_chiffrement_idea_long)
+            # ==================================================================================================
+            if cipher_mode == encryption_modes.ecb_encryption:
+                decipher_mode = encryption_modes.ecb_decryption
+            elif cipher_mode == encryption_modes.cbc_encryption:
+                decipher_mode = encryption_modes.cbc_decryption
+            elif cipher_mode == encryption_modes.pcbc_encryption:
+                decipher_mode = encryption_modes.pcbc_decryption
+            while True:
+                fichier_sortie = input("Veuillez rentrer le chemin pour le fichier de sortie "
+                                       "(une fois qu'il sera déchiffré).\n")
+                if os.path.isdir(os.path.dirname(fichier_sortie)):
+                    break
+                else:
+                    print("Le chemin vers le dossier de résultat est incorrect.")
+
+            dechiffrement_fichier(fichier_chiffre, fichier_sortie, decipher_mode, option6=True)
